9cd5c2379db83dbe2436405ed4ed85c3ea212d52
Suzan BULGAN <sbulgan@prismamedia.com>
2026-04-03 16:36:32 +0200

    fix url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
 
 app.use(cors({
-  origin: "http://localhost:3000/",
+  origin: "https://exam-blanc.onrender.com/",
   credentials: true
 }));
 
